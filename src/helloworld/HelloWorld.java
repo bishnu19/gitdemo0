74d85abfa326e297688a0de7e5e074abd848eb4a
bishnu19 <bishnu_bhusal83@yahoo.com>
2023-05-25 18:56:53 -0600

    code is modified

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,9 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World ");
-		System.out.println("Hello hello ");
+		
 		System.out.println("Java class");
 		System.out.println("git add");
+		
 
 	}
 
